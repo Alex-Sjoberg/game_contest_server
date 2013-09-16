f354507d1d46e79ad7d92e7672210368dd37b21a
Alex-Sjoberg <asjoberg22@yahoo.com>
2013-09-16 16:32:32 +0000

    Added signup path and database migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     attr_accessor :username , :password , :confirmation
+  
 	def new
         puts "Here"
         render text: params[:post].inspect
---
+++ db/migrate/20130916160741_add_stuff_to_users.rb
@@ -0,0 +1,7 @@
+class AddStuffToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_column :users, :password, :string
+    add_column :users, :confirmation, :string
+  end
+end
