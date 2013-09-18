5d68c8c0b56756ba663542069fe7bff1a65cc325
Alex-Sjoberg <asjoberg22@yahoo.com>
2013-09-18 16:10:53 +0000

    Changed some capitalizations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ActiveRecord::Base
   
-	def new
-        puts "Here"
-        render text: params[:post].inspect
-	end
     
     def create
     end
--- spec/requests/users_pages_spec.rb
+++ spec/requests/users_pages_spec.rb
@@ -4,9 +4,9 @@ describe "UsersPages" do
   describe "Sign Up" do
     it "allows the user to fill in name and password" do
       visit signup_path
-      fill_in 'username', with: 'user@example.com'
-      fill_in 'password', with: 'password'
-      fill_in 'confirmation', with: 'password'
+      fill_in 'Username', with: 'user@example.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Confirmation', with: 'password'
       click_button 'Create new account'
     end
   end
