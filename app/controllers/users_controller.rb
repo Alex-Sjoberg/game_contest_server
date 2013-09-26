7052bb941f2d7dfc41998436f77247c55277cd5f
Alex-Sjoberg <asjoberg22@yahoo.com>
2013-09-26 19:04:59 +0000

    Passing user validation tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,21 @@
 class UsersController < ApplicationController
     
     def new
-        @user = User.new()
+        @user = User.new
     end
     
     def create
-        puts "PARAMS IS!!!!!!!!!!" , params
-        puts "Params sub user" , params[:user]
-       # params = params[:user].symbolize_keys
-        
         @user = User.new(user_params)
-        if @user.save
+      if @user.save then
         #@user.update_attributes!(user_params)
           redirect_to @user
         else
           render 'new'
         end 
-    end
+      end 
+      def show
+        @user = User.find(params[:id])
+      end
     
     private 
     def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   
-  validates :username , presence: true , uniqueness: true
+  #validates :username , presence: true , uniqueness: true , length: {maximum: 20}
+  #validates :email, presence: true , uniqueness: true ,format: { with: /\A[\w\d]{1,8}@[\w\d]+\.[a-z]{2,}\z/ }
+  validates :username , presence: true , uniqueness:true ,length: {maximum: 20}
+  validates :email , presence:true , format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 end
