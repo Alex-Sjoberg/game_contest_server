d61ffcac33b18aa0e0d23f820f334177f168abf5
Alex-Sjoberg <asjoberg22@yahoo.com>
2013-09-30 22:02:41 +0000

    Passing edit tests. Some webgl testing in here

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,36 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+  
+  def index
+    @users = User.all
+  end
     
     def create
         @user = User.new(user_params)
       if @user.save then
-        #@user.update_attributes!(user_params)
           redirect_to @user
         else
           render 'new'
         end 
       end 
+       
       def show
+        @user = User.find(params[:id])
+      end
+      
+      
+      def update
+        @user = User.find(params[:id])
+       
+        if @user.update(user_params)
+          redirect_to @user
+        else
+          render 'edit'
+        end
+      end  
+      
+      def edit
         @user = User.find(params[:id])
       end
     
