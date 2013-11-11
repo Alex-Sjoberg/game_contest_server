a57b49b8401aa17ecefafa98dbef7b8c5c0571d1
Alex-Sjoberg <asjoberg22@yahoo.com>
2013-11-11 17:01:24 +0000

    changed gitignore

--- app/models/referee.rb
+++ app/models/referee.rb
@@ -8,8 +8,9 @@ class Referee < ActiveRecord::Base
             'then its an error!'
         else
             time_no_spaces = Time.now.to_s.gsub(/\s/,'_')
-            file_location = Rails.root.join('code','referees', time_no_spaces + current_user.id.to_s)
-            self.file_location = 'the/location/of/the/file/on/the/server/;/might/be/long/like/this/-Ghandi'
+            file_location = Rails.root.join('code','referees', Rails.env ,time_no_spaces).to_s
+            IO::copy_stream(uploaded_io, file_location)
+          self.file_location = file_location
         end
     end
 end
